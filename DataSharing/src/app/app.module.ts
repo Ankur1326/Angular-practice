58882590638026b51db6b55a4bb31017445c1630
Ankur1326 <swamiankur46@gmail.com>
2023-10-01 10:11:20 +0530

    first commit

---
+++ DataSharing/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TeacherComponent } from './teacher/teacher.component';
+import { StudentComponent } from './student/student.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TeacherComponent,
+    StudentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
