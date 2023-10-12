f5811897d7451f4ae522d8b7c499e31676fbb2cb
Ankur1326 <swamiankur46@gmail.com>
2023-10-12 22:34:50 +0530

    app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { FormsModule } from '@angular/forms';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
+    UsersComponent,
   ],
   imports: [
     BrowserModule,
