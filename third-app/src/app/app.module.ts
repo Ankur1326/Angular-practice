2d8faec929053b585946aa8930524feafe4d02a9
Ankur1326 <swamiankur46@gmail.com>
2023-10-20 22:17:44 +0530

    04:39:53 Template driven Form

--- third-app/src/app/app.module.ts
+++ third-app/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ third-app/src/app/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  email = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ])
+  
+  password = new FormControl("", [
+    Validators.required,
+    Validators.minLength(6)
+  ])
+
+  loginForm = new FormGroup({
+    email:this.email,
+    password:this.password
+  })
+
+  login() {
+    console.log(this.loginForm.value);
+  }
+
+}
